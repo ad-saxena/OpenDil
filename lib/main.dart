8ad6181a9efef8a70169cf70a68a4f65e8fc81b3
ADITI <aditi.ravichandran22@gmail.com>
2020-09-26 17:33:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:open_dil_hackathon/login_page.dart';
+
+void main() => runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage()
+    )
+);
+
