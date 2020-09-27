7b54a39cd88f03c455ee1247b2c94409ebbd3b46
ad saxena <hindiabcd1234@gmail.com>
2020-09-27 13:48:02 +0530

    Update the file

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/routes.dart';
-import 'package:shop_app/screens/splash/splash_screen.dart';
-import 'package:shop_app/theme.dart';
+import 'screens/sign_in/sign_in_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // ignore: todo
+  // TODO: implement createStat
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: theme(),
+      title: 'Help!',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignInScreen(),
       // home: SplashScreen(),
       // We use routeName so that we dont need to remember the name
-      initialRoute: SplashScreen.routeName,
-      routes: routes,
+      /*initialRoute: SplashScreen.routeName,
+      routes: routes,*/
     );
   }
 }
